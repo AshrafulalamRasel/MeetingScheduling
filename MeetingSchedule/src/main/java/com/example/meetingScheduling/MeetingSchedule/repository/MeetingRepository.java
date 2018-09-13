483a2ef8de841c3e7949d44897ce81db4aa29868
AshrafulalamRasel <ashrafulrasel21@gmail.com>
2018-09-13 15:10:16 +0600

    first commit

--- MeetingSchedule/src/main/java/com/example/meetingScheduling/MeetingSchedule/repository/MeetingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.meetingScheduling.MeetingSchedule.repository;
-
-import com.example.meetingScheduling.MeetingSchedule.modal.Meeting;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MeetingRepository extends JpaRepository<Meeting,Long> {
-}
